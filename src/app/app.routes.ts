41cded12a80b52f9f115638c320c6e32427b1d7f
enidanzaldo <anzaldoenid@gmail,com>
2025-09-19 21:41:31 -0600

    practica funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CounterPageComponent } from './pages/home-page/counter-page/counter-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: HomePageComponent},
+
+    {path: 'counter', component: CounterPageComponent},
+    
+    {path: '**', redirectTo: ''} // Wildcard route for a 404 page
+];
+
